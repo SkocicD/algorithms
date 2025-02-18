2978f20a66114ea8a19bba386e78f8bda5280fd6
SkocicD <skocicdavid@gmail.com>
2025-02-18 14:20:02 -0500

    suspension bridges no longer defeat me

--- kattis/medium/suspensionbridges/suspensionbridges.py
+++ kattis/medium/suspensionbridges/suspensionbridges.py
@@ -1,21 +1,24 @@
-import math
+from math import sinh, cosh
 
 d, s = [int(x) for x in input().split()]
 
 def f(a):
-    return a*math.cosh(d/(2*a))-a-s
+    return a*cosh(d/(2*a))-a-s
 
+a,b=(1,1)
+while f(a)<0 or f(b)>0:
+    a/=2 if f(a)<0 else a 
+    b*=2 if f(b)>0 else b
 
-tol = .000000000001
-diff = 1
-a,b = (d,1000)
-while abs(a-b)>tol:
-    c = (a+b)/2
+prevc = -1
+c = (a+b)/2
+while abs(1-c/prevc)>.00000000001:
     if f(a)*f(c)>0:
         a = c
     else:
         b = c
+    prevc = c
+    c = (a+b)/2
 
-c = (a+b)/2
-print(2*c*math.sinh(d/(2*c)))
+print(2*c*sinh(d/(2*c)))
 
