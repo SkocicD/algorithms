94e4fe0166d8e2b753dd2e5c303e8419881ee434
SkocicD <skocicdavid@gmail.com>
2025-03-25 21:00:24 -0400

    after collin

--- kattis/easy/ultimatebinarywatch/ultimatebinarywatch.py
+++ kattis/easy/ultimatebinarywatch/ultimatebinarywatch.py
@@ -1,2 +1,3 @@
-a,b,c,d=[f'{int(x):04b}' for x in input()]
-[print(f'{a[i]} {b[i]}   {c[i]} {d[i]}'.translate({49:42,48:46})) for i in range(4)]
+a, b, c, d = [f'{int(x):04b}'for x in input()]
+[print(f'{a[i]} {b[i]}   {c[i]} {d[i]}'.translate({49: 42, 48: 46}))
+ for i in range(4)]
