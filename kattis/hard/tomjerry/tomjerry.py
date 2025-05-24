1acaa9ebbb7d3bab18b8b4cc13347f7901dc4381
SkocicD <skocicdavid@gmail.com>
2025-05-23 22:27:08 -0400

    test case 33 TLE

---
+++ kattis/hard/tomjerry/tomjerry.py
@@ -0,0 +1,73 @@
+from math import comb
+from functools import cache
+
+
+def isInside(cheese1, cheese2):
+    return cheese1[0] <= cheese2[0] and cheese1[1] <= cheese2[1] and cheese1 != cheese2
+
+
+X, Y = list(map(int, input().split()))
+
+cheeses = []
+numcheeses = int(input())
+for _ in range(numcheeses):
+    cheeses.append(tuple(map(int, input().split())))
+cheeses = sorted(cheeses, key=lambda t: (t[0], t[1]), reverse=True)
+
+end = (X, Y)
+# tree = {"maxdepth": 1}
+tree = {}
+
+
+def addtotree(currtree, cheese):
+    # currtree[cheese] = {"maxdepth": 1}
+    currtree[cheese] = {}
+    for treecheese in currtree:
+        # if treecheese == "maxdepth":
+        #     continue
+        if isInside(cheese, treecheese):
+            addtotree(currtree[treecheese], cheese)
+            # if currtree[treecheese]["maxdepth"] > currtree["maxdepth"]:
+            #     currtree["maxdepth"] = currtree[treecheese]["maxdepth"]
+
+
+for cheese in cheeses:
+    addtotree(tree, cheese)
+
+# print(tree)
+
+
+@cache
+def paths_between(inner, outer):
+    n = outer[0]-inner[0]+outer[1]-inner[1]
+    k = min(outer[0]-inner[0], outer[1]-inner[1])
+    return comb(n, k)
+
+
+total = 0
+
+
+def traverse(tree, depth=0, product=1, parent=end):
+    global total
+    if tree == {}:
+        return
+    for cheese in tree:
+        currproduct = product
+        currproduct *= paths_between(cheese, parent)
+        traverse(tree[cheese], depth + 1, currproduct, cheese)
+        therest = currproduct*paths_between((1, 1), cheese)
+        if depth % 2 == 0:
+            total += therest
+        else:
+            total -= therest
+
+
+traverse(tree)
+
+print(total % 1000000007)
+
+# #     for cheese in cheeses:
+# #     xcheese, ycheese = cheese
+# #     n1 = xcheese + ycheese - 2
+# #     k1 = min(xcheese, ycheese)-1
+# # print(total % 1000000007)
