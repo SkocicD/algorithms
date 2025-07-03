1accabe674513ed4c25e6a161b84b87cc2759eaa
SkocicD <skocicdavid@gmail.com>
2025-07-03 12:58:57 -0400

    solved gourmeten again with dp

--- kattis/easy/gourmeten/gourmeten.py
+++ kattis/easy/gourmeten/gourmeten.py
@@ -1,39 +1,11 @@
-from math import factorial
-from collections import defaultdict
-from collections import Counter
+mins = int(input())
+meals = []
+for _ in range(int(input())):
+    meals.append(int(input()))
 
-
-def step(remain, sequence, counts, options, start):
-    if remain < 0:
-        return 0
-    if remain == 0:
-        # print(sequence)
-        # print(counts)
-        num = factorial(len(sequence))
-        for key in counts:
-            num //= factorial(counts[key])
-            num *= maincounts[key[1]] ** counts[key]
-        # print(num)
-        return num
-
-    total = 0
-    for i, opt in options[start:]:
-        sequence.append(opt)
-        counts[(i, opt)] += 1
-        total += step(remain-opt, sequence, counts, options, i)
-        sequence.pop(-1)
-        counts[(i, opt)] -= 1
-    return total
-
-
-m = int(input())
-n = int(input())
-times = []
-for i in range(n):
-    times.append(int(input()))
-maincounts = Counter(times)
-# print(maincounts)
-times = list(enumerate(list(set(times))))
-
-
-print(step(m, [], defaultdict(int), times, 0))
+times = [1] + [0] * mins
+for m in range(mins):
+    for t in meals:
+        if m + t < len(times):
+            times[m+t] += times[m]
+print(times[-1])
