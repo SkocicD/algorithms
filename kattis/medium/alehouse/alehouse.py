05bfd80a6fbc6c3392ce2647f4f8708f336355c8
SkocicD <skocicdavid@gmail.com>
2025-07-13 18:58:23 -0400

    alehouse completed (binary search w/ overlapping ranges)

--- kattis/medium/alehouse/alehouse.py
+++ kattis/medium/alehouse/alehouse.py
@@ -1,13 +1,25 @@
-from collections import defaultdict
+from bisect import bisect_right, bisect_left
+n, k = list(map(int, input().split()))
 
-n,k = list(map(int,input().split()))
-
-ppl = defaultdict(set)
+starts = []
+ends = []
+all = []
 
 for j in range(n):
-    a,b = list(map(int,input().split()))
-    for i in range(a,b):
-        ppl[i].add(j)
+    a, b = list(map(int, input().split()))
+    starts.append(a)
+    ends.append(b)
+    all.append(a)
+    all.append(b)
 
-for val in ppl:
-    print(ppl[val])
+starts = sorted(starts)
+ends = sorted(ends)
+mx = 0
+# find the number of ranges that start before its endpoint and subtract the number that end before its start point
+for i in range(2*n):
+    high = all[i]
+    low = high - k
+    a = bisect_right(starts, high)  # number that start before the end point
+    b = bisect_left(ends, low)  # number that end before the start point
+    mx = max(mx, a-b)
+print(mx)
