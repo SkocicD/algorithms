44722627101105119c5321483e29e98ab6fb0ad1
SkocicD <skocicdavid@gmail.com>
2025-07-13 18:57:01 -0400

    opening ceremony finally defeated (binary search)

--- kattis/medium/ceremony/ceremony.py
+++ kattis/medium/ceremony/ceremony.py
@@ -1,35 +1,21 @@
-from collections import Counter, OrderedDict
-from itertools import pairwise
-n=input()
-h=list(map(int,input().split()))
-h=dict(sorted(dict(Counter(h)).items()))
+from bisect import bisect_right
+input()
+h = sorted(map(int, input().split()))
 
-for a,b in pairwise(h):
-    h[b] += h[a]
-
-a,b=(0,len(h)-1)
-
-while a!=b:
-    # print("a,b=",a,b)
-    i = (a+b)//2
-    keys = list(h.keys())
-    height = keys[i]
-    num_below = h[height]
-    # print(i,height,num)
-    if height > num_below:
-        b = i
+total = 0
+last = len(h)
+cut = 0
+low = 0
+while last - low > 0:
+    # find the index that is the number of buildings (last-low) plus how many floors have been cut off of it (+cut)
+    # add cut because if there are 4 buildings left and they all had 2 cut off, we need some that were originally 6 (4+2)
+    i = bisect_right(h, last-low+cut, hi=last)
+    # if there exist any towers taller than the number ofbuildings left, add this number of buildings to the total
+    if i < last:
+        total += last-i
+        last = i
     else:
-        a = i+1
-
-keys = list(h.keys())
-height = keys[a]
-num_below = h[height]
-
-if a == 0 and height > num_below:
-    print(h[keys[-1]])
-elif a == len(keys)-1 and height < num_below:
-    print(keys[-1])
-else:
-    print(a)
-    print(keys[a-1])
-    print(keys[a-1]+h[keys[-1]]-h[keys[a-1]])
+        cut += 1
+        total += 1
+        low = bisect_right(h, cut)
+print(total)
