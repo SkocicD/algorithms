39834f7902ea0ec5c67f6f54ad9339f14e49bdc5
SkocicD <skocicdavid@gmail.com>
2026-01-18 02:23:01 -0500

    golf 248

--- kattis/easy/timebomb/timebomb.py
+++ kattis/easy/timebomb/timebomb.py
@@ -1,29 +1,8 @@
-m = [245, 80, 103, 87, 210, 151, 183, 84, 247, 215]
-s = [0]*10
-for j in range(10):
-    n = m[j]
-    i = 0
-    while n:
-        b = n & 1
-        s[j] |= 7*b << (6*i) if i < 4 else 73*b << 2*(i % 2)+6*(i > 5)
-        n >>= 1
-        i += 1
-
-a = 0
-for r in open(0):
-    k = int(len(r)/4)
-    if not a:
-        a = ['']*k
-    for i in range(0, k):
-        a[i] += r[i*4:i*4+3]
-for i in range(len(a)):
-    a[i] = int(a[i].replace("*", "1").replace(" ", "0"), 2)
-
-num = 0
-for i in range(1, len(a)+1):
-    if a[-i] not in s:
-        num = 1
-        break
-    else:
-        num += s.index(a[-i])*(10**(i-1))
-print('BEER!!' if num % 6 == 0 else 'BOOM!!')
+c=[32319,31,24253,22207,28831,30391,32439,16927,32447,30399]
+b=list(map(''.join,zip(*open(0))))
+x=0
+while b:
+ if(d:=int(''.join('01'[c>' ']for c in''.join(b[:3])),2))not in c:x=1;break
+ x=x*10+c.index(d)
+ b=b[4:]
+print('BOOM!!'if x%6 else'BEER!!')
