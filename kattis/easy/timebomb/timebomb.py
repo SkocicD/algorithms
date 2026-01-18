46d259010343ebe7f1be03d2d45d58e22c64218c
SkocicD <skocicdavid@gmail.com>
2026-01-18 02:59:40 -0500

    golf 233

--- kattis/easy/timebomb/timebomb.py
+++ kattis/easy/timebomb/timebomb.py
@@ -1,8 +1,6 @@
-m=''.join
 c=[32319,31,24253,22207,28831,30391,32439,16927,32447,30399]
-b=[*map(m,zip(*open(x:=0)))]
+b=[*map(m:=''.join,zip(*open(x:=0)))]
 while b:
  if(d:=int(m('01'[c>' ']for c in m(b[:3])),2))not in c:x=1;break
- x=x*10+c.index(d)
- b=b[4:]
+ x=x*10+c.index(d);b=b[4:]
 print('BOOM!!'if x%6else'BEER!!')
