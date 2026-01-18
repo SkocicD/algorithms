a537cbd6f742bb3e1efe9210602c184566b80cf9
SkocicD <skocicdavid@gmail.com>
2026-01-18 02:38:04 -0500

    golf 236

--- kattis/easy/timebomb/timebomb.py
+++ kattis/easy/timebomb/timebomb.py
@@ -1,7 +1,6 @@
 m=''.join
-x=0
 c=[32319,31,24253,22207,28831,30391,32439,16927,32447,30399]
-b=list(map(m,zip(*open(0))))
+b=[*map(m,zip(*open(x:=0)))]
 while b:
  if(d:=int(m('01'[c>' ']for c in m(b[:3])),2))not in c:x=1;break
  x=x*10+c.index(d)
